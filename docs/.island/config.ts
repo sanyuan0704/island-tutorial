b10285356cec204b4c5ea546c82a06ed6a9a89d8
sanyuan <494130947@qq.com>
2022-10-10 09:44:32 +0800

    feat: init structure

--- docs/.island/config.ts
+++ docs/.island/config.ts
@@ -3,7 +3,15 @@ import { remarkCodeHike } from "@code-hike/mdx";
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
-const theme = require("shiki/themes/min-light.json"); // any theme from shiki
+const theme = require("shiki/themes/nord.json"); // any theme from shiki
+
+function getI18nHelper(lang: "zh" | "en") {
+  const cn = lang === "zh";
+  const prefix = cn ? "/zh" : "/en";
+  const getLink = (str: string) => `${prefix}${str}`;
+  const getText = (cnText: string, enText: string) => (cn ? cnText : enText);
+  return { getText, getLink };
+}
 
 export default defineConfig({
   markdown: {
@@ -13,18 +21,12 @@ export default defineConfig({
   },
   title: "Island.js 教程",
   themeConfig: {
-    sidebar: {
-      "/guide/": [
-        {
-          text: "目录",
-          items: [
-            {
-              text: "初始化项目",
-              link: "/",
-            },
-          ],
-        },
-      ],
+    outline: false,
+    locales: {
+      "/": {
+        nav: getNavbar("zh"),
+        sidebar: getSidebar(),
+      },
     },
   },
   enableSpa: true,
@@ -32,3 +34,71 @@ export default defineConfig({
     exclude: ["compoents/**/*"],
   },
 });
+
+function getNavbar(lang: "zh" | "en") {
+  const { getLink, getText } = getI18nHelper(lang);
+
+  return [
+    {
+      text: getText("指南", "Guide"),
+      link: "https://island.sanyuan0704.top/zh/guide/getting-started",
+    },
+    {
+      text: getText("交互式教程", "Tutorial"),
+      link: "/",
+    },
+    {
+      text: getText("API", "API"),
+      link: "https://island.sanyuan0704.top/zh/api/",
+    },
+  ];
+}
+
+function getSidebar() {
+  return {
+    "/guide/": [
+      {
+        text: "文档站搭建",
+        items: [
+          {
+            text: "初始化项目",
+            link: "/",
+          },
+          {
+            text: "导航栏/侧边栏配置",
+            link: "/config-bar",
+          },
+          {
+            text: "编写 Markdown 内容",
+            link: "/markdown",
+          },
+          {
+            text: "项目构建打包",
+            link: "/build",
+          },
+          {
+            text: "接入国际化",
+            link: "/i18n",
+          },
+          {
+            text: "项目部署",
+            link: "/deploy",
+          },
+        ],
+      },
+      {
+        text: "高级应用",
+        items: [
+          {
+            text: "自定义主题",
+            link: "/custom-theme",
+          },
+          {
+            text: "交互式文档开发",
+            link: "/code-hike",
+          },
+        ],
+      },
+    ],
+  };
+}
